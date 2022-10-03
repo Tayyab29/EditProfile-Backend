44f3659452f7fc908fb663ea61e934960819d728
Tayyab <xhahg32@gmail.com>
2022-10-03 14:12:08 +0500

    Fully Updated

--- src/edit-profile/edit-profile.controller.ts
+++ src/edit-profile/edit-profile.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { EditProfileService } from './edit-profile.service';
 import { CreateEditProfileDto } from './dto/create-edit-profile.dto';
 import { UpdateEditProfileDto } from './dto/update-edit-profile.dto';
+import { DateSearchQuery } from './dto/date-search-query.dto';
 
 @Controller('edit-profile')
 export class EditProfileController {
@@ -17,13 +26,36 @@ export class EditProfileController {
     return this.editProfileService.findAll();
   }
 
+  @Get('count')
+  findDataLenghth() {
+    return this.editProfileService.findDataLenghth();
+  }
+
+  @Post('/filter')
+  findAllData(@Body() Date: DateSearchQuery) {
+    return this.editProfileService.findAllData(Date);
+  }
+
+  // @Get('/count')
+  // filterDate(@Query() filterdto: DateSearchQuery) {
+  //   console.log(filterdto);
+  //   if (Object.keys(filterdto).length) {
+  //     return this.editProfileService.filterDate(filterdto);
+  //   } else {
+  //     return 'Nothing to show';
+  //   }
+  // }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.editProfileService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEditProfileDto: UpdateEditProfileDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateEditProfileDto: UpdateEditProfileDto,
+  ) {
     return this.editProfileService.update(+id, updateEditProfileDto);
   }
 
